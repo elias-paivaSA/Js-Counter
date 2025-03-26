45b36c29131bdbd0a527f9d736fe1c943c8268b5
elias-paivaSA <eliaspsa13@gmail.com>
2025-03-26 17:08:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,18 +7,20 @@ const reset = document.getElementById("reset");
 
 reset.addEventListener("click", (event) => {
     if (number.innerHTML !== 0) number.innerHTML = 0;
-    if (number.innerHTML == 0) number.style.color = "black"
+        number.style.color = "black"
 });
 
 increase.addEventListener("click", (event) => {
     if (number.innerHTML >= 0 || number.innerHTML < 0) number.innerHTML++
-    if (number.innerHTML > 0) number.style.color = "purple"
+    if (number.innerHTML > 0) number.style.color = "purple";
+    if (number.innerHTML == 0) number.style.color = "black";
 });
 
 decrease.addEventListener("click", (event) => {
     if (number.innerHTML <= 0 || number.innerHTML > 0)
         number.innerHTML--
-    if (number.innerHTML < 0) number.style.color = "red"
+    if (number.innerHTML < 0) number.style.color = "red";
+    if (number.innerHTML == 0) number.style.color = "black";
 });
 
 // && and operator
